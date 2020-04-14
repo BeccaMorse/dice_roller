79f6d04fec6509c2bd7d35d1fdbc40b8b9b9bd32
Russell Emmer <me@russelleh.info>
2020-04-14 10:11:59 -0400

    Add second die

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var dieRoller = require('./dieRoller.js');
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("d6").addEventListener("click", () => {
-        document.getElementById("output").innerHTML
-            = dieRoller();
+        var dice = document.getElementsByClassName("output");
+        for (var i = 0; i < dice.length; i++) {
+            dice.item(i).innerHTML = dieRoller();
+        }
     })
 });

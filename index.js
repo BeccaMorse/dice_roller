e4b60f3b44b0bb4a8579540f645f24d9e8522317
Russell Emmer <me@russelleh.info>
2020-04-14 09:59:10 -0400

    Call testable function to roll die

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+var dieRoller = require('./dieRoller.js');
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("d6").addEventListener("click", () => {
         document.getElementById("output").innerHTML
-            = Math.ceil(Math.random() * 6)
+            = dieRoller();
     })
 });
